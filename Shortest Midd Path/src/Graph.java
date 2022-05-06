7abe7fc1fa023fab250c58f83b93b4c9f6a0fcd1
katie11mac <77913668+katie11mac@users.noreply.github.com>
2022-05-06 13:00:43 -0400

    Add files via upload

--- Shortest Midd Path/src/Graph.java
+++ Shortest Midd Path/src/Graph.java
@@ -25,6 +25,7 @@ import java.util.Map;
 
 public class Graph {
 
+	// ***GIVEN***
 	// Object that contains an adjacency list of a road network and a dictionary from elements of the list 
 	// to indices from 0 to |V|-1, since the roads are labeled in the data by arbitrary indices. 
 	// Because we are considering a walking application, we construct the adjacency list so that 
@@ -33,9 +34,14 @@ public class Graph {
 	// In other words, the adjacency matrix that is already written here is a reverse adjacency matrix.
 	HashMap<Integer, ArrayList<Road>> adjList;
 	HashMap<Integer,Integer> nodeDict;
-	HashMap<Integer, Integer> inverseNodeDict; // the values of nodeDict are now the keys and the keys are now the values 
+	
+	
+	// ADDED ATTRIBUTE: If given the value of a vertex according to nodeDict, we can find it's node number in the graph. 
+	//					Essentially the reverse of nodeDict, where the values are now the keys and the keys are now the values. 
+	HashMap<Integer, Integer> inverseNodeDict; 
 
 
+	// ***GIVEN*** 
 	public Graph(String file) throws IOException{
 		// We will store the information about the road graph in an adjacency list
 		// We will use a HashMap to store the Adjacency List, since each vertex in the graph has a more or less random integer name.
@@ -43,9 +49,6 @@ public class Graph {
 		adjList = new HashMap<>();
 		nodeDict = null;
 
-	// so the adjlist's key = node and value is the list of roads connected to that road (the roads are the edges of the graph) 
-	// i don't understand the point of creating a nodeDict if every node has a distinct number like why? 
-		
 		// Based on https://stackoverflow.com/questions/49599194/reading-csv-file-into-an-arrayliststudent-java
 		String line = null;
 		BufferedReader br = new BufferedReader(new FileReader(file));
@@ -70,7 +73,10 @@ public class Graph {
 			j++;
 		}
 		
-		//
+		// ADDED METHOD: 
+		// Fill in the inverseNodeDict with the same values of nodeDict, but now the keys of nodeDict are now the values of 
+		// inverseNodeDict and the values of nodeDict are now the keys of inverseNodeDict. 
+		// This allows us to easily node number in the graph if given the value of that node in nodeDict. 
 		inverseNodeDict = new HashMap<>(); 
 		int i = 0 ;
 		for (Integer nodeNum: nodeDict.keySet()) {
@@ -81,7 +87,7 @@ public class Graph {
 	}
 
 
-	// get functions
+	// ***GIVEN*** get functions
 	public HashMap<Integer, ArrayList<Road>> getAdjList(){
 		return adjList;
 	}
@@ -93,6 +99,7 @@ public class Graph {
 	}
 
 
+	//***GIVEN***
 	public synchronized void addToAdjList(Road road) {
 		//Adds the Road (edge) to the appropriate list of the adjacency list. 
 		//This method is used by the constructor method
@@ -116,87 +123,77 @@ public class Graph {
 	
 	/**
 	 * 
-	 * This method should create the array storing the objective function values of subproblems used in Bellman Ford. 
+	 * Create the array storing the objective function values of subproblems used in Bellman Ford. 
 	 * 
 	 * @param startNode
-	 * @return
+	 * @return array dpArray filled with the minimum distance of each subproblem in Bellman Ford
 	 */
     public Double[][] ShortestDistance(Integer startNode){
     	
+    	// Initialize the array which will store the objective function values 
     	int numNodes = adjList.size(); 
     	int maxNumEdges = numNodes; // bc well be including 0 
-		Double[][] dpArray = new Double[numNodes][maxNumEdges]; // Nodes are the rows and edges are the colums 
+		Double[][] dpArray = new Double[numNodes][maxNumEdges]; 
 		
 		// Initialize the nxn array with infinities 
-		//Read more: https://javarevisited.blogspot.com/2015/09/how-to-loop-two-dimensional-array-in-java.html#ixzz7Rnl7ZLo1
+		// 		Based on: https://javarevisited.blogspot.com/2015/09/how-to-loop-two-dimensional-array-in-java.html#ixzz7Rnl7ZLo1
 		for (int row = 0; row < dpArray.length; row++) { 
 			for (int col = 0; col < dpArray[row].length; col++) { 
 				dpArray[row][col] = Double.POSITIVE_INFINITY;
 			} 
 		}
 
-		
-		// startNode does not line up with the value of the index for which they are stored in the 2D array 
+		// Need to convert the given startNode ID to its corresponding index in the dpArray using nodeDict
 		int dictStartNode = nodeDict.get(startNode); 
-		System.out.println("NODE: " + startNode + " DICT NODE: " + dictStartNode); 
+//		System.out.println("NODE: " + startNode + " DICT NODE: " + dictStartNode); 
 		
-		//System.out.println("Dictionary Start Node: " + dictStartNode); 
+		// Distance from a the starting vertex to itself with zero edges is a distance of 0 
 		dpArray[dictStartNode][0] = 0.0; 
 		
-		
-		//go through the edges 
+		// Iterate through all the possible edges first 
+		// Start at 1 because if you start at zero then you're looking for a path from s to some vertex 
+		// with 0 edges. That is impossible, so there is no path meaning that it's distance will remain infinity. 
 		for(int i = 1; i < maxNumEdges; i++) { 	
 			
-			// going through every vertex 
+			// Iterate through every vertex (v is the index where the node's obj func values is stored at in dpArray) 
 			for(int v = 0; v < numNodes; v++) {
-				//System.out.print(inverseNodeDict.get(v)); 
-				// need to convert back to the actual node's id bc the index that its stored at is not the same 
+				
+				// Need to convert back to the node's ID 
 				int nodeID = inverseNodeDict.get(v); 
-				//System.out.println("NODE: " + nodeID + " v: " + v); 
 				ArrayList<Road> nodeAdjList = adjList.get(nodeID); 
-				System.out.println("*********************************************"); 
-				System.out.println("ADJ LIST FOR " + nodeID + ": " + nodeAdjList); 
+//				System.out.println("*********************************************"); 
+//				System.out.println("ADJ LIST FOR " + nodeID + ": " + nodeAdjList); 
+//				System.out.println("CURRENT VALUE: " + dpArray[v][i]); 
 				
-				System.out.println("CURRENT VALUE: " + dpArray[v][i]); 
+				dpArray[v][i] = dpArray[v][i-1]; // One of the final options for obj function 
 				
-//				if (i > nodeAdjList.size()) {
-//					dpArray[v][i] = dpArray[v][i-1];
-//					//System.out.println("THIS IS THE NEW VALUE: " + dpArray[v][i]);	
-//				} else {
-					
-				dpArray[v][i] = dpArray[v][i-1]; 
+				// Only want to go through the adj list (neighboring nodes) to save time
 				for (Road road : nodeAdjList) {
-		
-					int u = nodeDict.get(road.getEnd()); // value from 0 to 192 
-					System.out.println("StartNode: " + nodeID + " (v: "+ v +  ") EndNode: " + road.getEnd() + " (u: " + u + ")"); 
-					System.out.println(dpArray[u][i-1] + " " + road.getMiles()); 
-					System.out.println("IS THIS VALUE LESS: " + (dpArray[u][i-1] + road.getMiles())); 
+					int u = nodeDict.get(road.getEnd()); // the node you will be visiting before v 
+//					System.out.println("StartNode: " + nodeID + " (v: "+ v +  ") EndNode: " + road.getEnd() + " (u: " + u + ")"); 
+//					System.out.println(dpArray[u][i-1] + " " + road.getMiles()); 
+//					System.out.println("IS THIS VALUE LESS: " + (dpArray[u][i-1] + road.getMiles())); 
 					
-					//if(dpArray[u][i-1] + road.getMiles() < dpArray[v][i]) {
-					// it keeps adding this road.getMiles() when the previous entry already holds the getMiles 
+					// Want to check if the other final option is less than the final option currently stored 
 					if(dpArray[u][i-1] + road.getMiles() < dpArray[v][i]) {
-						// INFINITY 
-						// check what happens when dpArray[u][i-1]
-						System.out.println("NEW ENTRY FOR " + v + " " + i); 
-						//dpArray[v][i] = dpArray[u][i-1] + road.getMiles(); 
 						dpArray[v][i] = dpArray[u][i-1] + road.getMiles(); 
-						System.out.println("THIS IS THE NEW VALUE: " + dpArray[v][i]);
-						System.out.println(); 
+//						System.out.println("NEW ENTRY FOR " + v + " " + i); 
+//						System.out.println("THIS IS THE NEW VALUE: " + dpArray[v][i]);
+//						System.out.println(); 
 					} 
-					System.out.println(); 
-					
+//					System.out.println(); 
 				}
 			}
-			
 		}
 		 
-		System.out.println(Arrays.deepToString(dpArray));
+//		System.out.println(Arrays.deepToString(dpArray));
+		
 		return dpArray;
     }
 
     /**
      * 
-     * This method should work backwards through the array you created in ShortestDistance and output the 
+     * Work backwards through the array created in ShortestDistance and output the 
      * sequence of streets you should take to get from your starting point to your ending point.
      * 
      * @param endNode
@@ -204,44 +201,57 @@ public class Graph {
      */
     public void ShortestPath(Integer endNode, Double[][] dpArray){
 
-    	// need to get the corresponding index in the dpArray for the endNode 
+    	// Need to get the corresponding index in the dpArray for the endNode 
     	int dictEndNode = nodeDict.get(endNode);
-    	//System.out.println(endNode + " " + dictEndNode); 
     	
-    	int i = (dpArray.length) - 1; 
-    	//System.out.println("i is " + i); 
+    	// Using i and v to track our position in A as we backtrack 
+    	// i is the max number of edges and v is the vertex we will currently be evaluating 
+    	int i = (dpArray.length) - 1;  
     	int v = dictEndNode; 
-    	ArrayList<Road> path = new ArrayList<Road>(); // this could be an array instead to save space  
+    	ArrayList<Road> path = new ArrayList<Road>();
     	
-    	System.out.print("END NODE: " + endNode + " || DISTANCE: ");
-    	System.out.println(dpArray[dictEndNode][i]); // this is saying that the distance is two miles 
+//    	System.out.print("END NODE: " + endNode + " || DISTANCE: " + dpArray[dictEndNode][i]);
     	
-    	
+    	// EDGE CASE: There is no path from startNode to endNode
     	if (dpArray[dictEndNode][i] == Double.POSITIVE_INFINITY) {
-    		System.out.println("No Path"); 
+    		System.out.println("There is no path :("); 
     	}
     	
-    	// i think that there are some repeated roads because of the fact that the i is so high
-    	// it will run until it hits 1
+    	// While we have more than 0 edges allowed 
     	while (i > 0) { 
+    		// First check if the optimal path is less than i edges. 
+    		// If it is not, then we find what the ith edge is 
     		if (dpArray[v][i] != dpArray[v][i-1]) {
-    			// want to go through the adjacency list instead to save sometime 
-    			ArrayList<Road> nodeAdjList = adjList.get(inverseNodeDict.get(v)); 
-    			//System.out.println("Node: " + v + " Adj List: " + nodeAdjList); 
     			
+    			ArrayList<Road> nodeAdjList = adjList.get(inverseNodeDict.get(v));
+    			
+    			// Want to go through the adj list (neighboring edges) to save sometime 
     			for (Road road : nodeAdjList) {
-    				int u = nodeDict.get(road.getEnd());
+    				
+    				int u = nodeDict.get(road.getEnd()); // The potential node we'd be visiting before v 
+    				
     				if(dpArray[v][i] == dpArray[u][i-1] + road.getMiles()) {
-    					path.add(0, road); // add this road at the beginning of the array
-    					v = u; 
+    					path.add(0, road); // Add this road at the beginning of the list 
+    					v = u; // Update the node that we are currently at 
     					break; 
+    					// We can break out of loop because we've found the ith edge and don't need 
+    					// to check any further edges 
     				}
+    				
     			}
+    			
     		}
+    		
     		i--; 
+    		
     	}
-    	System.out.println(path); 
-    	System.out.println(); 
+    	
+//    	System.out.println(path); 
+    	
+    	// Print out the street names for the path 
+    	for (Road road : path) {
+    		System.out.println(road.getName()); 
+    	}
     	
 	}			
 				
--- Shortest Midd Path/src/Main.java
+++ Shortest Midd Path/src/Main.java
@@ -28,47 +28,17 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 
 		// The following creates an adjacency list data structure that stores the vertices and edges in the graph. 
-		// (I have written this part of the algorithm for you.)
-		// You may have to adjust the file address in the following line to your computer
 		Graph graph = new Graph("./src/Data/VT_Road_Centerline.csv");
-		
-		//System.out.println("Graph adjList: " + graph.getAdjList()); 
-		//System.out.println("Graph nodeDict: " + graph.getDict()); 
-		//System.out.println("Graph inverseNodeDict: " + graph.getInverseNodeDict()); 
-		// The following should implement the array-filling part of the Bellman Ford algorithm 
-		// for all points on the graph, starting at node 53980
-		// 53980 is the closest node to 75 Shannon St. (You will need to modify it in the file Graph.java)
+	
+		// The following should implement the array-filling part of the Bellman Ford algorithm for all points on the graph, starting at node 53980
+		// 53980 is the closest node to 75 Shannon St.
 		Double[][] dpArray = graph.ShortestDistance(53980);
 
-		System.out.println("You should take the following walking route to get from 75 Shannon to Green Peppers:");
+		System.out.println("You should take the following walking route to get from 75 Shannon to Green Peppers:\n");
 		graph.ShortestPath(30783,dpArray);
-		graph.ShortestPath(53983, dpArray);
-		graph.ShortestPath(5878, dpArray);
+		System.out.println("\n***NOTE: Some streeet names are repeated because the street is broken up into different sections in the data.***\n"); 
 
 	}
-
-	/*
-	 * What should the output of the path look like? 
-	 * I'm not too picky, but something like, a list of street names, 
-	 * separated by commas, that you could follow to get from 75 Shannon to your destination.
-	 * 
-	 * How do I find other places to test? 
-	 * Go to Vermont Geodata Portal, and click on the road that your destination on. 
-	 * You next need to figure out if your destination is closest to the start or end id. 
-	 * The problem is that you don't know which end of the road is the start and which is the end, 
-	 * so go to a neighboring road and see which start id or end id it has in common.
-	 * 
-	 * Is the adjacency list a reverse or regular list? 
-	 * As discussed in the code, the graph is undirected, so the adjacency list is 
-	 * both a regular and reverse list at the same time.
-	 * 
-	 * I'm getting repeated street names - what is going on? 
-	 * As long as you understand why they are repeated, you can leave them in the output. 
-	 * Looking at street names on the Vermont Geodata Portal at the link above might be helpful.
-	 */
-
-
-
 	
 
 }
